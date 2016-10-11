a9322c4bb549a6ccddd7232048315138d5a46f75
Liran Sharir <lsharir@gmail.com>
2016-10-11 19:09:51 +0300

    normal appCtrl, siftSvc

--- angular1/src/app/app.js
+++ angular1/src/app/app.js
@@ -3,121 +3,105 @@ import angular from 'angular';
 import '../style/app.scss';
 
 let app = () => {
-  return {
-    template: require('./app.html'),
-    controller: 'AppCtrl',
-    controllerAs: '$ctrl'
-  }
+	return {
+		template: require('./app.html'),
+		controller: 'AppCtrl',
+		controllerAs: '$ctrl'
+	}
 };
 
 class AppCtrl {
-  constructor($scope, SiftService) {
-    let ctrl = this;
+	constructor($scope, SiftService) {
+		let ctrl = this,
+			sift = SiftService;
 
-    this.name = 'Liran Sharir';
+		this.$scope = $scope;
+		this.sift = SiftService;
+		this.name = 'Liran Sharir';
+		this.rawSubjects = require('../../../shared/subjects.js');
+		this.rawCategories = require('../../../shared/categories.js');
+		this.data = {
+			subjects: [],
+			categories: [],
+			subjectsByType: []
+		};
+		this.selectedTags = [];
+		this.defaultTags = [
+			{ text: '2016', active: false },
+			{ text: 'angular2', active: false },
+			{ text: 'experience', active: false }
+		];
 
-    let rawSubjects = require('../../../shared/subjects.js'),
-      rawCategories = require('../../../shared/categories.js');
+		$scope.query = '';
 
-    this.data = {
-      subjects: [],
-      categories: [],
-      subjectsByType: []
-    };
+		$scope.$watch('query', function (newValue) {
+			let tags = sift.getTags(newValue, ctrl.selectedTags, ctrl.defaultTags);
+			ctrl.filterResume(tags);
+		});
+	}
 
-    this.selectedTags = [
+	addTag(tag) {
+		if (tag.length !== 0) {
+			let exists = false;
 
-    ];
+			this.selectedTags.forEach(existingTag => {
+				exists = exists || existingTag === tag;
+			});
 
-    this.defaultTags = [
-      { text: '2016', active: false },
-      { text: 'angular2', active: false },
-      { text: 'experience', active: false }
-    ];
+			if (!exists) {
+				this.selectedTags.push(tag);
+			}
+		}
+	}
 
-    this.focusOnInput = function () {
-      document.getElementById('search').focus();
-    };
+	inputKeyUp(e) {
+		if (e.keyCode === 32 || e.keyCode === 13) {
+			let tags = this.$scope.query.split(' ');
+			tags.forEach(tag => {
+				this.addTag(tag);
+			});
+			this.$scope.query = '';
+		}
 
-    this.inputKeyUp = function (e) {
-      if (e.keyCode === 32 || e.keyCode === 13) {
-        let tags = $scope.query.split(' ');
-        tags.forEach(tag => {
-          addTag(tag);
-        });
-        $scope.query = '';
-      }
+		if (e.keyCode === 8 && this.$scope.query.length === 0) {
+			this.selectedTags.splice(this.selectedTags.length - 1, 1);
+			this.filterResume(this.selectedTags);
+		}
+	}
 
-      if (e.keyCode === 8 && $scope.query.length === 0) {
-        ctrl.selectedTags.splice(ctrl.selectedTags.length - 1, 1);
-      }
-    };
+	focusOnInput() {
+		document.getElementById('search').focus();
+	}
 
-    function addTag(tag) {
-      if (tag.length !== 0) {
-        let exists = false;
+	toggleDefault(tag) {
+		tag.active = !tag.active;
+		this.filterResume(this.sift.getTags(undefined, this.selectedTags, this.defaultTags));
+	}
 
-        ctrl.selectedTags.forEach(existingTag => {
-          exists = exists || existingTag === tag;
-        });
+	filterResume(tags) {
+		let years = this.sift.getYears(tags),
+			yearsLength = years.length,
+			regularTags = this.sift.getRegularTags(tags),
+			newData = {
+				subjects: [],
+				categories: [],
+				subjectsByType: []
+			}
+		;
 
-        if (!exists) {
-          ctrl.selectedTags.push(tag);
-        }
-      }
-    }
+		this.rawCategories.forEach(category => {
+			newData.categories.push(category);
+		});
 
-    $scope.query = '';
+		this.rawSubjects.forEach(subject => {
+			if (this.sift.inYear(subject, years, yearsLength)) {
+				this.sift.addSubjectByType(newData.subjectsByType, subject);
+				newData.subjects.push(subject);
+			}
+		});
 
-    $scope.$watch('query', function (newValue) {
-
-      let tags = newValue.split(' ');
-
-      
-      // let newData = {
-      //   subjects: [],
-      //   categories: [],
-      //   subjectsByType: []
-      // },
-      //   yearMatches = newValue.match(/\b([0-9]{4})\b/g),
-      //   years = yearMatches ? yearMatches.map(year => Number(year)) : [],
-      //   yearsLength = years.length,
-      //   tags = newValue.split(' ');
-      // ;
-
-      // ctrl.tags = tags;
-
-      // rawSubjects.forEach(subject => {
-      //   if (inYear(subject, years, yearsLength)) {
-      //     addSubjectByType(newData.subjectsByType, subject);
-      //     newData.subjects.push(subject);
-      //   }
-      // });
-
-      // rawCategories.forEach(category => {
-      //   newData.categories.push(category);
-      // });
-
-      // ctrl.data = newData;
-    });
-
-    function addSubjectByType(subjectsByTypeArray, subject) {
-      if (!subjectsByTypeArray[subject.type] || !subjectsByTypeArray[subject.type].length) {
-        subjectsByTypeArray[subject.type] = [];
-      }
-      
-      subjectsByTypeArray[subject.type].push(subject);
-    }
-
-    function inYear(subject, years, yearsLength) {
-      for (let i = 0; i < yearsLength; i++) {
-        if (subject.start > years[i] || subject.end < years[i]) {
-          return false;
-        }
-      }
-      return true;
-    }
-  }
+		this.data = newData;
+	};
 }
 
 import { category } from './components/category/category.component.js';
@@ -129,13 +113,13 @@ import { SiftService } from './services/sift.service.js';
 const MODULE_NAME = 'resume';
 
 angular.module(MODULE_NAME, [require('angular-animate')])
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl)
-  .component('category', category)
-  .component('subject', subject)
-  .component('tag', tag)
-  .filter('subjectYears', subjectYears)
-  .service('SiftService', SiftService)
-  ;
+	.directive('app', app)
+	.controller('AppCtrl', AppCtrl)
+	.component('category', category)
+	.component('subject', subject)
+	.component('tag', tag)
+	.filter('subjectYears', subjectYears)
+	.service('SiftService', SiftService)
+	;
 
 export default MODULE_NAME;
--- angular1/src/app/services/sift.service.js
+++ angular1/src/app/services/sift.service.js
@@ -3,7 +3,75 @@ export class SiftService {
 
     }
 
-    generateTags () {
-        
+    getTags(text, tags, defaults) {
+        let textTags = text !== undefined ? text.split(' ') : [],
+            validatedTags = [];
+
+        textTags.forEach(tag => {
+            if (this.validateTag(tag)) {
+                validatedTags.push(tag);
+            }
+        })
+
+        defaults.forEach(tag => {
+            if (tag.active) {
+                validatedTags.push(tag.text);
+            }
+        });
+
+        tags = validatedTags.concat(tags);
+
+        return tags;
+    }
+
+    validateTag(tag) {
+        return tag.length !== 0 && tag.indexOf(' ') === -1;
+    }
+
+    getYears(tags) {
+        let years = [];
+
+        if (!tags || !tags.length) {
+            return years;
+        }
+
+        tags.forEach(tag => {
+            let matches = tag.match(/\b([0-9]{4})\b/g);
+            if (matches && matches.length !== 0) {
+                years.push(Number(tag));
+            }
+        });
+
+        return years;
+    }
+
+    getRegularTags(tags) {
+        let regular = [];
+
+        tags.forEach(tag => {
+            let matches = tag.match(/\b([0-9]{4})\b/g);
+            if (!matches) {
+                regular.push(tag);
+            }
+        });
+
+        return regular;
+    }
+
+    inYear(subject, years, yearsLength) {
+        for (let i = 0; i < yearsLength; i++) {
+            if (subject.start > years[i] || subject.end < years[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    addSubjectByType(subjectsByTypeArray, subject) {
+        if (!subjectsByTypeArray[subject.type] || !subjectsByTypeArray[subject.type].length) {
+            subjectsByTypeArray[subject.type] = [];
+        }
+
+        subjectsByTypeArray[subject.type].push(subject);
     }
 }
